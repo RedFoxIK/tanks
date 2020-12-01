ff2835d7c4561c9ea60b1e4366d84aa204b55caa
Yaroslava Kalashnyk <kalashnyky@mydigicode.com>
2020-12-01 08:27:56 +0200

    preload screen is ready

--- src/app/gameState.ts
+++ src/app/gameState.ts
@@ -3,14 +3,8 @@ import images from '../assets/images/board/*.png';
 import bonuses from '../assets/images/bonus/*.png';
 import sounds from '../assets/sounds/*.wav';
 import otherImages from '../assets/other/*.png';
-import screens from '../assets/screenes/*.png';
-// import btn from '../assets/images/loader_bar/button.png';
-import loaderBarPath from '../assets/images/loader_bar/loader-bar.png';
-import loaderBgPath from '../assets/images/loader_bar/loader-bg.png';
 import * as PIXI from 'pixi.js';
 import {ImagePath} from "./model/imagePath";
-
-// import imagePath from '../assets/images/board';
 
 export interface GameState {
     load(gameApp: GameApp);
@@ -18,13 +12,11 @@ export interface GameState {
 
 export class LoadGame implements GameState {
     load(gameApp: GameApp) {
-        console.info('Start loading');
-        console.log('enum = ' + ImagePath.allBoardElems())
-
         const stage = gameApp.app.stage;
 
         gameApp.loader
             .add(Object.values(images))
+            .add('startBtn', 'assets/images/loader_bar/button.png')
             .add(ImagePath.all)
             .add(Object.values(sounds))
             .add(Object.values(bonuses))
@@ -32,40 +24,48 @@ export class LoadGame implements GameState {
 
         gameApp.loader.load();
 
-        const progressBar = new PIXI.Graphics();
-        stage.addChild(progressBar);
+        const title = new PIXI.Text('Tank Game', {fontSize: 100, fontFamily: 'Snippet', fill: 'white'});
+        title.position.x = 280;
+        title.position.y = 200;
+        stage.addChild(title);
 
-        const progressBarBorder = new PIXI.Graphics();
+        const progressBarBorder = new PIXI.Sprite(PIXI.Texture.from('assets/images/loader_bar/loader-bg.png'));
+        progressBarBorder.x = 200;
+        progressBarBorder.y = 500;
+        progressBarBorder.width = 600;
+        progressBarBorder.height = 80
         stage.addChild(progressBarBorder);
-        progressBar.beginFill(0x000000);
-        progressBar.drawRect(210, 498, 604, 64);
-        progressBar.endFill();
 
+        const progressBarBg = new PIXI.Sprite(PIXI.Texture.from('assets/images/loader_bar/loader-bar.png'));
+        progressBarBg.x = 205;
+        progressBarBg.y = 505;
+        progressBarBg.width = 0;
+        progressBarBg.height = 70;
+        stage.addChild(progressBarBg);
 
         gameApp.loader.onProgress.add(e => {
-            progressBar.beginFill(0xFFFF00);
-            progressBar.drawRect(212, 500, e.progress * 6, 60);
-            progressBar.endFill();
+            progressBarBg.width = 590 * (e.progress * 0.01);
             gameApp.app.renderer.render(stage);
         })
 
-        gameApp.loader.onComplete.add(e => {
-            console.log('DISPLAY BUTTON');
-            stage.removeChild(progressBar);
+        gameApp.loader.onComplete.add((loader, resources) => {
+            stage.removeChild(progressBarBorder, progressBarBg);
 
-            const button = new PIXI.Sprite(PIXI.Texture.from(ImagePath.START_BTN));
-            // const button2 = new PIXI.Sprite(PIXI.Texture.from(ImagePath.START_BTN));
-            button.x = 10;
-            button.y = 15
+            const button = new PIXI.Sprite(resources.startBtn.texture);
+            button.x = 350;
+            button.y = 450;
+
+            button.interactive = true;
+            button.buttonMode = true;
+
+            button.on("pointerdown", () => {
+                stage.removeChild(button, title);
+            })
+
             gameApp.app.stage.addChild(button);
 
         })
-
-        gameApp.loader.onError.add(e => {
-            console.log(e);
-        })
     }
-
 }
 
 export class PlayGame
